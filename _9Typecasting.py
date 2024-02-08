01b0b71f6a8a5d19dd2fb6ee32f2836ceb24a13f
IshaKumari10 <isha02740@gmail.com>
2024-02-08 21:45:34 +0530

    till user input

---
+++ _9Typecasting.py
@@ -0,0 +1,19 @@
+a="1"
+# a=1
+# b=2
+b="2"
+print(int(a)+int(b))
+
+#EXAMPLE OF EXPLICIT TYPECASTING:-
+string="15"
+number=7
+string_number=int(string)
+sum=number+string_number
+print("The sum of both the numbers:",sum)
+
+
+
+#EXAMPLE OF IMPLICIT TYPECASTING:-
+c=1.9
+d=8
+print(c+d)
