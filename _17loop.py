ee09ada7f20803cd8114a9cd4d6fc27dd03b6fa5
IshaKumari10 <isha02740@gmail.com>
2024-02-13 22:25:11 +0530

    till loop

---
+++ _17loop.py
@@ -0,0 +1,51 @@
+# name="Abhishek"
+# for i in name:
+#     print(i)
+#     if(i=="b"):
+#         print("This is somethig special!")/
+
+
+
+# colors=["Red","Yellow","Green","Blue"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#       print(i)
+
+
+
+
+# for i in range(5):
+#     print(i+1)
+
+
+# for i in range(1,20001):
+#     print(i)
+
+
+# for i in range(1,12,3):
+#     print(i)
+
+
+
+# i=0
+# while(i<=3):
+#     print(i)
+#     i=i+1
+
+
+
+# i=int(input("Enter the number:"))
+# while(i<=38):
+#     i=int(input("Enter the number:"))
+#     print(i)
+# print("Done with the loop")
+
+
+
+# count=-5
+# while(count>0):
+#     print(count)
+#     count=count-1
+# else:
+#     print("I am inside else")
