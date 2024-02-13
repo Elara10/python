c32b8ae0a37d5d1c56f03b65b59c2621f7357935
IshaKumari10 <isha02740@gmail.com>
2024-02-13 20:02:30 +0530

    if else

--- _12conditionals.py
+++ _12conditionals.py
@@ -1,6 +1,51 @@
-a=int(input("Enter your age"))
-print("Your age is:",a)
-if(a>18):
-   print("You can drive")
+# a=int(input("Enter your age"))
+# print("Your age is:",a)
+# if(a>18):
+#    print("You can drive")
+# else:
+#    print("You cannot drive")
+
+
+# if else  elif--1
+
+# applePrice=10
+# budget=200
+# if(budget-applePrice>590):
+#     print("Alexa,add 1 kg Apples to the cart.")
+# elif(budget-applePrice>70):
+#     print("Its okay you can buy")
+# else:
+#     print("Alexa,do not add Apples to the cart.")
+
+
+# if else elif--2
+
+# num=int(input("Enter your number"))
+# if(num<0):
+#     print("Number is negative.")
+# elif(num==0):
+#     print("Number is zero.")
+# elif(num==999):
+#     print("Number is special.")
+# else:
+#     print("Number is positive.")
+# print("I am happy now")
+
+
+
+
+
+# NESTED IF
+num=int(input("Enter a number"))
+if(num<0):
+   print("Number is negative")
+elif(num>0):
+    if(num<=10):
+        print("Number is between 1-10")
+    elif(num>10  and num<=20):
+        print("Number is between 11-20")
+    else:
+        print("Number is greater than 20")
 else:
-   print("You cannot drive")
+    print("Number is zero")
+    
