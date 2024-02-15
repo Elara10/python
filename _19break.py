337a0cbc125608521dd2c3970a4b45a5819ff6de
IshaKumari10 <isha02740@gmail.com>
2024-02-15 21:52:34 +0530

    till function

---
+++ _19break.py
@@ -0,0 +1,24 @@
+# BREAK
+
+# for i in range(12):
+#   if(i==7):
+#     print("leave the loop")
+#     break
+#   print("5 X",i,"=",5*i)
+
+
+
+
+# CONTINUE
+
+# for i in range(12):
+#   if(i==7):
+#     print("Skip the iteration")
+#     continue
+#   print("5 X",i,"=",5*i)
+
+
+
+# DO WHILE LOOP
+
+
---
+++ _20function.py
@@ -0,0 +1,37 @@
+# def calculateGmean(a,b):
+#     mean=(a*b)/(a+b)
+#     print(mean)
+# a=9
+# b=8
+# # gmean1=(a*b)/(a+b)
+# # print(gmean1)
+# calculateGmean(a,b)
+# c=8
+# d=7
+# # gmean2=(c*d)/(c+d)
+# # print(gmean2)
+# calculateGmean(c,d)
+
+
+
+def calculateGmean(a,b):
+  mean=(a*b)/(a+b)
+  print(mean)
+
+def isGreater(a,b):
+   if(a>b):
+    print("First number is greater")
+   else:
+    print("Second number is greater")
+
+def isLesser(a,b):
+    pass
+e=4
+f=5
+isGreater(e,f)
+calculateGmean(e,f)
+
+g=8
+h=7
+isGreater(g,h)
+calculateGmean(g,h)
